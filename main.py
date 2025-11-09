ddc7ad7931befca1d9fe6e7e60a90befe9cd53fb
WWWEqqq870 <h59146083@gmail.com>
2025-11-09 02:10:36 -0500

    FINAL FIX: Cleaned up all rfc3161ng import issues.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request
 from pymongo import MongoClient
 from rfc3161ng import get_timestamp
-from rfc3161ng.errors import RFC3161Error, HTTPError # استيراد RFC3161Error و HTTPError
+from rfc3161ng import RFC3161Error, HTTPError # استيراد RFC3161Error و HTTPError
 import datetime, hashlib, os, joblib, numpy as np
 # Final fix to trigger redeploy
 app = FastAPI()
